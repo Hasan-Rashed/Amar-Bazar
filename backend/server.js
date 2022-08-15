77770444642c5b247e8a1d11b43597be6d76c8e2
Hasan-Rashed <hasanrashed537@gmail.com>
2022-08-15 20:36:16 +0600

    MongoDB Server Setup

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
 const app = require('./app');
 const dotenv = require('dotenv');
+const connectDatabase = require('./config/database')
+
 
 // config
 /* Loading the config.env file. */
 dotenv.config({path: 'backend/config/config.env'});
+
+
+// connecting to Database
+connectDatabase();
+
 
 
 /* Listening to the port that is specified in the config.env file. */
